8dba2e0fbb5850c484a7593e70459d5726c45c00
titledev <title@hpktechnology.com>
2024-12-05 16:50:49 +0700

    update

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+#Set up the environment
+export PRINT_ENV=Dev ENDPOINT=srv+mongodb://nongkai.com PORT=5000
+
 # Run the application
 cargo build --release
+
+# Run the application
+./target/release/rust-demo
